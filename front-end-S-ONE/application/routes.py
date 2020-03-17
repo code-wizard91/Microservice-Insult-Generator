5b22189574417adb94ca4b57b67878274f3d87ec
code-wizard91 <alimizan@hotmail.com>
2020-03-17 10:27:22 -0400

    added file changes

--- front-end-S-ONE/application/routes.py
+++ front-end-S-ONE/application/routes.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 from application import app
-from flask import render_template, url_for, redirect, request,
+from flask import render_template, url_for, redirect, request
 import requests
 import random
 
 
-@app.route('/', methods=['GET', POST])
+@app.route('/', methods=['GET'])
 def home():
 	insult = "You are a snot nosed donkey"
 	return render_template('home.html',insult = insult, title = 'Home')
--- meme-generator-S-FIVE/application/routes.py
+++ meme-generator-S-FIVE/application/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from application import app
-from flask import render_template, url_for, redirect, request,
+from flask import render_template, url_for, redirect, request
 import requests
 import random
 
