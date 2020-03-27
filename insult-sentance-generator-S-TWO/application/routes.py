6af20dff63f1978dc18188f4869d7923cbcb1a5e
Mahboob Ali <alimizan@hotmail.com>
2020-03-27 17:00:19 +0000

    Update routes.py

--- insult-sentance-generator-S-TWO/application/routes.py
+++ insult-sentance-generator-S-TWO/application/routes.py
@@ -8,6 +8,6 @@ import random
 @app.route('/insultsentance', methods=['GET'])
 def insultsentance():
 
-	list = ['You are a','Hey you absolute','You ugly long nosed','Your cat is a','How can you 		be such a monkey brained','You had to be such a mental','Why are you always a crazy','Why 		you insolent']
+	list = ['You are a','Hey you absolute','You ugly long nosed','Your cat is a','How can you be such a monkey brained','You had to be such a mental','Why are you always a crazy','Why you insolent']
 	
 	return list[random.randrange(7)]
