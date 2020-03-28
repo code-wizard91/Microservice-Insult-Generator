3eedb309ce25d3207633fe0031a4b6ae30ba4885
Mahboob Ali <alimizan@hotmail.com>
2020-03-28 16:28:48 +0000

    Update routes.py

--- insultjoiner-S-FOUR/application/routes.py
+++ insultjoiner-S-FOUR/application/routes.py
@@ -7,8 +7,8 @@ import random
 
 @app.route('/specialinsult', methods=['GET'])
 def specialinsult():
-	response1 = requests.get('http://localhost:5001/insultsentance')
-	response2 = requests.get('http://localhost:5002/insultword')
+	response1 = requests.get('http://insultservicetwo:5001/insultsentance')
+	response2 = requests.get('http://insultservicethree:5002/insultword')
 	response3 = response1.text+" "+response2.text
 	return response3
 
