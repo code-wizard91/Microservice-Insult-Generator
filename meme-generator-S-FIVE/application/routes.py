82dd571f69897d76b5cb2db9b00fd04a873f74e0
Mahboob Ali <alimizan@hotmail.com>
2020-04-05 16:21:37 +0100

    Add files via upload

---
+++ meme-generator-S-FIVE/application/routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from application import app
+from flask import render_template, url_for, redirect, request
+import requests
+import random
+
+
+@app.route('/meme', methods=['GET'])
+def meme():
+	url = "https://joke3.p.rapidapi.com/v1/joke"
+	headers = {
+	'x-rapidapi-host': "joke3.p.rapidapi.com",
+	'x-rapidapi-key': "e6adf9892amsh8b30e72a8871cf6p14c10djsna97888a775b9"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+	return response
