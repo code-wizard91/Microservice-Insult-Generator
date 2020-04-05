612630436b5ee4f52ce2269303f8dc500ed9637a
Ubuntu <mizan@manger.3apikbhev5uerefjksrn1mg3tg.zx.internal.cloudapp.net>
2020-04-05 16:07:48 +0000

    updated final service

--- meme-generator-S-FIVE/application/routes.py
+++ meme-generator-S-FIVE/application/routes.py
@@ -7,12 +7,12 @@ import random
 
 @app.route('/meme', methods=['GET'])
 def meme():
-	url = "https://joke3.p.rapidapi.com/v1/joke"
-	headers = {
-	'x-rapidapi-host': "joke3.p.rapidapi.com",
-	'x-rapidapi-key': "e6adf9892amsh8b30e72a8871cf6p14c10djsna97888a775b9"
+    url = "https://joke3.p.rapidapi.com/v1/joke"
+    headers = {
+        'x-rapidapi-host': "joke3.p.rapidapi.com",
+        'x-rapidapi-key': "e6adf9892amsh8b30e72a8871cf6p14c10djsna97888a775b9"
     }
 
     response = requests.request("GET", url, headers=headers)
 
-	return response
+    return response
