c39fd5a13a02db0f0cd5d5f7401350685e8debb5
Mahboob Ali <alimizan@hotmail.com>
2020-04-16 17:00:20 +0100

    Update routes.py

--- front-end-S-ONE/application/routes.py
+++ front-end-S-ONE/application/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-from application import app,db
-from application.models import Insults
+from application import app#,db
+#from application.models import Insults
 from flask import render_template, url_for, redirect, request
 import requests
 import random
@@ -12,7 +12,7 @@ def home():
         jokeres = requests.get('http://insultjoinerservicefive:5004/meme')
         jokerstr = jokeres.text
         insult = response.text
-        insultgen = Insults(insult=response.text)
-        db.session.add(insultgen)
-        db.session.commit()
+        #insultgen = Insults(insult=response.text)
+        #db.session.add(insultgen)
+        #db.session.commit()
         return render_template('home.html',jokerstr = jokerstr,insult = insult, title = 'Home')
